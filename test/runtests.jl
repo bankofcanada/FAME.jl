9ba86cb835d08e430b45fdb5c9b5a7e1adebb4e0
Boyan Bejanov <bbejanov@gmail.com>
2022-01-05 10:08:08 -0500

    Fixed up tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,6 +6,13 @@ using FAME
 
 using TimeSeriesEcon
 
+if FAME.chli.lib === nothing
+
+    @info "FAME CHLI library not found"
+    exit()
+
+end
+
 @testset begin
     w = Workspace(; a = 1, b = TSeries(2020Q1, randn(10)),
         s = MVTSeries(2020M1, (:q, :p), randn(24, 2)),
@@ -18,22 +25,22 @@ using TimeSeriesEcon
     tmp = readfame("data.db")
     @test tmp isa Workspace && length(tmp) == 7
     @test issubset(keys(tmp), (:a, :b, :c_alpha, :c_beta, :c_n_s, :s_p, :s_q))
-    
+
     tmp = readfame("data.db", "s?")
     @test tmp isa Workspace && length(tmp) == 2
     @test issubset(keys(tmp), (:s_p, :s_q))
 
-    tmp = readfame("data.db", "s?", prefix="s")
+    tmp = readfame("data.db", "s?", prefix = "s")
     @test tmp isa Workspace && length(tmp) == 2
     @test issubset(keys(tmp), (:p, :q))
 
-    tmp = readfame("data.db", "c?", collect="c")  
+    tmp = readfame("data.db", "c?", collect = "c")
     @test tmp isa Workspace && length(tmp) == 1
     @test issubset(keys(tmp), (:c,))
     @test tmp.c isa Workspace && length(tmp.c) == 3
     @test issubset(keys(tmp.c), (:alpha, :beta, :n_s))
-    
-    tmp = readfame("data.db", collect=["c"=>["n"], "s"])
+
+    tmp = readfame("data.db", collect = ["c" => ["n"], "s"])
     @test tmp isa Workspace && length(tmp) == 4
     @test issubset(keys(tmp), (:a, :b, :c, :s))
     @test tmp.c isa Workspace && length(tmp.c) == 3
@@ -42,4 +49,7 @@ using TimeSeriesEcon
     @test issubset(keys(tmp.c.n), (:s,))
     @test tmp.s isa Workspace && length(tmp.s) == 2
     @test issubset(keys(tmp.s), (:p, :q))
+
+    rm("data.db")
+
 end
